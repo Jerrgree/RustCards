3efcb9082b3eaeaaf26ae9c2f8ba1e074b52f353
Jerrgree <jeremyrgreenburg@gmail.com>
2024-03-01 18:31:47 -0600

    Add player

---
+++ src/player.rs
@@ -0,0 +1,15 @@
+use crate::card::Card;
+
+pub struct Player {
+    pub hand: Vec<Card>,
+    pub name: String,
+}
+
+impl Player {
+	pub fn new(name: String) -> Player {
+		Player {
+			name: name,
+			hand: Vec::<Card>::with_capacity_in(10);
+		}
+	}
+}
